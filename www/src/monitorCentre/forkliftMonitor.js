267e624e3dfb76e37a140eb6f8dec0fead962b73
L <L@jia-wifi.local>
2016-11-05 22:59:04 +0800

    add table in forkliftMonitor

--- www/src/monitorCentre/forkliftMonitor.js
+++ www/src/monitorCentre/forkliftMonitor.js
@@ -6,6 +6,38 @@
 
         function forkliftMonitorCtrl($scope, Service) {
             var vm = this;
+            //是否全选属性
+            vm.allChecked = false;
+            //单选
+            vm.checked = checked;
+            //全选
+            vm.checkAll = checkAll;
+
+
+            //被选中的叉车ID
+            vm.checkedId = '';
+
+            //获取叉车列表数据
+            getTableData();
+
+
+
+            //单选
+            function checked(item) {
+            	vm.checkedId = vm.checkedId == item?'':item;
+            }
+
+            //全选
+            function checkAll() {
+                vm.allChecked = !vm.allChecked;
+            }
+
+            //获取叉车列表数据
+            function getTableData() {
+                Service.getJson('forkliftMonitor').then(function(data) {
+                    vm.tableData = data;
+                });
+            }
         }
     });
 })();
