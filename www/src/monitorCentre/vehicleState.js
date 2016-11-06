e19fd22a8e58ab1c63cbc73d79eff3f8e185fcf6
fangrong <15915878706@163.com>
2016-11-06 12:04:12 +0800

    车辆状态

--- www/src/monitorCentre/vehicleState.js
+++ www/src/monitorCentre/vehicleState.js
@@ -3,10 +3,43 @@
     define(['app'], function(app) {
         app.controller('vehicleStateCtrl', vehicleStateCtrl);
 
-        vehicleStateCtrl.$inject = ['$scope'];
+        vehicleStateCtrl.$inject = ['$scope','Service'];
+        
 
-        function vehicleStateCtrl($scope) {
+        function vehicleStateCtrl($scope, Service) {
+            var vm = this;
+            //是否全选属性
+            vm.allChecked = false;
+            //单选
+            vm.checked = checked;
+            //全选
+            vm.checkAll = checkAll;
 
+
+            //被选中的叉车ID
+            vm.checkedId = '';
+
+            //获取叉车列表数据
+            getTableData();
+
+
+
+            //单选
+            function checked(item) {
+            	vm.checkedId = vm.checkedId == item?'':item;
+            }
+
+            //全选
+            function checkAll() {
+                vm.allChecked = !vm.allChecked;
+            }
+
+            //获取叉车列表数据
+            function getTableData() {
+                Service.getJson('vehicleStateCtrl').then(function(data) {
+                    vm.tableData = data;
+                });
+            }
         }
     });
 })();
